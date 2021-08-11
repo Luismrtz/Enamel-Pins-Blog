f4201c65b0f026eacde6f5a0fa9ef3272c8c2396
luisricardo494 <luisricardo494@gmail.com>
2021-08-11 18:49:34 -0400

    changed package versions

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCTvGcT3qiPLPSw_3Da21ZlRYBwdLi65e4",
+    authDomain: "enamelpinsblogs.firebaseapp.com",
+    projectId: "enamelpinsblogs",
+    storageBucket: "enamelpinsblogs.appspot.com",
+    messagingSenderId: "469123144218",
+    appId: "1:469123144218:web:1650d8aecfc7b1354e0fa3"
+  };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const timestamp = firebase.firestone.FieldValue.serverTimestamp;
+
+  export { timestamp };
+  export default firebaseApp.firestore();
